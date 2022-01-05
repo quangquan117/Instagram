d384055e637cb9e7b5b7173c69dd0475889d7db7
Leo Langlois <leo.langlois2@gmail.com>
2022-01-05 17:48:42 +0100

    [Modif] style.css and js

--- js/insta.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", e => {
-    console.log("here\n");
-
-})
---
+++ js/login.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", e => {
+    console.log("here\n");
+    let email, warning, mdp, submit, success;
+    submit = document.getElementsByTagName("input")[2];
+
+    localStorage.setItem("login", "playgame@gmail.com");
+    localStorage.setItem("mdp", "Play 200 games");
+    let login = () => {
+        warning = document.querySelector(".warning");
+        success = document.querySelector("success");
+        email = document.getElementsByTagName("input")[0].value;
+        mdp = document.getElementsByTagName("input")[1].value;
+
+        if (email == localStorage.getItem("login") && mdp == localStorage.getItem("mdp")) {
+            document.location.assign("accueil.html");
+        } else {
+            warning.style.display = "block";
+            warning.textContent = "Mail ou mot de passe incorrecte";
+        }
+    };
+    submit.addEventListener("click", event => {
+        event.preventDefault();
+        login();
+    });
+})
